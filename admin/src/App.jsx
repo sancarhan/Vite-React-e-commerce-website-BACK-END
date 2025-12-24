d49fa4a6eaf40874321c5e95df81595247e2cfcf
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2025-12-24 19:15:59 +0300

    .

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+      <Navbar/>
+      <hr className='border border-gray-300 border-b-0'/>
+      <div className='flex w-full'>
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+          <Routes>
+            <Route path='/add' element={ <Add/> }/>
+            <Route path='/list' element={ <List/> }/>
+            <Route path='/list' element={ <Orders/> }/>
+          </Routes>
+        </div>
+      </div>
+      </>
+       
     </div>
   )
 }
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-})
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
