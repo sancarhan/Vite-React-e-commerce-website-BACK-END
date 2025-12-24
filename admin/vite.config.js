d3930b851f2eee4c76ac2dd40c0fa0373a78cf3c
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2025-12-24 17:14:22 +0300

    .

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  server: {port:5173}
+  plugins: [react()],
+  server: {port: 5174}
 })
